a356119f2a1687051632cc3bc18ca9f3335c7b39
Alex Hampu <alex.hampu@deploy.com.ro>
2022-05-25 11:20:44 +0300

    update route parameter to allow for arrays

--- src/Module/DTO/MenuItemDTO.php
+++ src/Module/DTO/MenuItemDTO.php
@@ -4,11 +4,20 @@ namespace Hito\Core\Module\DTO;
 
 class MenuItemDTO
 {
+    public string $route;
+    public mixed $routeParams = [];
+
     public function __construct(public string            $name,
-                                public string            $route,
+                                string|array      $route,
                                 public string            $icon,
                                 public string|array|null $permission = null,
                                 public bool              $isActive = false)
     {
+        if (is_array($route) && count($route) === 2) {
+            $this->route = $route[0];
+            $this->routeParams = $route[1];
+        } elseif(is_string($route)) {
+            $this->route = $route;
+        }
     }
 }
